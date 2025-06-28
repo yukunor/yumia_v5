8f918b5b5a27192e8626843059b68e49f74b79c8
yukunor <noriyukikondo99@outlook.jp>
2025-06-28 22:48:10 +0900

    Update response_index.py

--- module/response/response_index.py
+++ module/response/response_index.py
@@ -11,9 +11,9 @@ def is_similar_composition(current, target):
     # 感情構成比のキーが完全一致しているか
     if set(current.keys()) != set(target.keys()):
         return False
-    # 各構成比の乖離が10%以内か
+    # 各構成比の乖離が15%以内か
     for key in current:
-        if abs(current[key] - target[key]) > 10:
+        if abs(current[key] - target[key]) > 15:
             return False
     return True
 
