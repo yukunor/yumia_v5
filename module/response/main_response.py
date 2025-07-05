84366e5d52cc04023472e8a155626451902589f4
yukunor <noriyukikondo99@outlook.jp>
2025-07-05 19:38:00 +0900

    Update main_response.py

--- module/response/main_response.py
+++ module/response/main_response.py
@@ -1,7 +1,8 @@
 from llm_client import generate_emotion_from_prompt_simple as estimate_emotion, generate_emotion_from_prompt_with_context, extract_emotion_summary
-from response.response_index import load_and_categorize_index, extract_best_reference
+from response.response_index import load_and_categorize_index, extract_best_reference, find_best_match_by_composition  # ← これを追加
 from utils import logger
 import json
+
 
 
 def load_emotion_by_date(path, target_date):
