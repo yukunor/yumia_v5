f98f653f7f5a8aac6bb92091bcd894decf1804f4
yukunor <noriyukikondo99@outlook.jp>
2025-07-12 10:39:42 +0900

    Update modules.py

--- module/db/modules.py
+++ module/db/modules.py
@@ -1,13 +1,13 @@
-from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 from datetime import datetime
 
 Base = declarative_base()
 
 class Message(Base):
-    __tablename__ = 'messages'
+    __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True, index=True)
     role = Column(String, nullable=False)
     message = Column(String, nullable=False)
-    timestamp = Column(DateTime, default=datetime.now)
+    timestamp = Column(DateTime, default=datetime.utcnow)
