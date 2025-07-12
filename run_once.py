ec91708b273d66b54b3932650243c0cd22b7aa6b
yukunor <noriyukikondo99@outlook.jp>
2025-07-12 09:29:36 +0900

    Update run_once.py

--- run_once.py
+++ run_once.py
@@ -1,4 +1,9 @@
-# run_once.py
+import sys
+import os
+
+# 現在のディレクトリのパス（project/src）をPythonパスに追加
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from db.database import init_db
 
 init_db()
