ca450b7069149b8ac4c997f80403d83ad3a584eb
yukunor <noriyukikondo99@outlook.jp>
2025-07-12 10:14:59 +0900

    Update run_once.py

--- module/db/run_once.py
+++ module/db/run_once.py
@@ -4,6 +4,6 @@ import os
 # src をパスに追加（project/src/module/db/ → project/src へ）
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
-from module.db.database import init_db
+from .database import init_db
 
 init_db()
