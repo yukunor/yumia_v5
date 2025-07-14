ba14762ae0ff4a234889bd78f4d5ba5312c0cb75
yukunor <noriyukikondo99@outlook.jp>
2025-07-14 12:59:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from fastapi import FastAPI
+from module.response.main_response import run_response_pipeline
 
-from pymongo import MongoClient
-import certifi
+app = FastAPI()
 
-uri = "mongodb+srv://noriyukikondo99:Aa1192296!@cluster0.oe0tni1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-client = MongoClient(uri, tlsCAFile=certifi.where())
-
-try:
-    print(client.list_database_names())  # データベース一覧を取得
-    print("✅ MongoDB Atlas への接続成功")
-except Exception as e:
-    print("❌ 接続失敗:", e)
+@app.post("/chat")
+def chat_endpoint(user_input: str):
+    response, emotion_data = run_response_pipeline(user_input)
+    return {"response": response, "emotion_data": emotion_data}
