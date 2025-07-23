aa9aa6690d25cbc90f4601308e27c4e51d69eb0a
yukunor <noriyukikondo99@outlook.jp>
2025-07-23 09:21:24 +0900

    Create response_loader.py

---
+++ module/response/response_loader.py
@@ -0,0 +1,16 @@
+# module/response/response_loader.py
+
+from module.response.response_short import find_history_by_emotion_and_date as find_short
+from module.response.response_intermediate import find_history_by_emotion_and_date as find_intermediate
+from module.response.response_long import find_history_by_emotion_and_date as find_long
+
+def collect_all_category_responses(emotion_name: str, date_str: str) -> dict:
+    short_data = find_short(emotion_name, "short", date_str)
+    intermediate_data = find_intermediate(emotion_name, "intermediate", date_str)
+    long_data = find_long(emotion_name, "long", date_str)
+
+    return {
+        "short": short_data,
+        "intermediate": intermediate_data,
+        "long": long_data
+    }
