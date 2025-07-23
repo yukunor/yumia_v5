6cbb854702018fae708053d124b03a8b74558c2e
yukunor <noriyukikondo99@outlook.jp>
2025-07-23 17:04:50 +0900

    Update utils.py

--- module/utils.py
+++ module/utils.py
@@ -124,5 +124,7 @@ def append_history(role, message):
 
 
 if __name__ == "__main__":
+    print("=== Logger Test Start ===", flush=True)
     logger.debug("🌟 デバッグ動作確認")
     logger.info("🔔 通常の情報ログ")
+    print("=== Logger Test End ===", flush=True)
