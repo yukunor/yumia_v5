03120731053adad450026348a53633fdab86dc98
yukunor <noriyukikondo99@outlook.jp>
2025-07-23 17:46:46 +0900

    Update response_loader.py

--- module/response/response_loader.py
+++ module/response/response_loader.py
@@ -3,6 +3,7 @@
 from module.response.response_short import find_history_by_emotion_and_date as find_short
 from module.response.response_intermediate import find_history_by_emotion_and_date as find_intermediate
 from module.response.response_long import find_history_by_emotion_and_date as find_long
+from module.utils.utils import mongo_logger as logger
 
 def collect_all_category_responses(emotion_name: str, date_str: str) -> dict:
     short_data = find_short(emotion_name, "short", date_str)
