7bca34fa171e7502449cd74926529c95b99cbfee
yukunor <noriyukikondo99@outlook.jp>
2025-07-23 09:21:59 +0900

    Update llm_client.py

--- module/llm/llm_client.py
+++ module/llm/llm_client.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from module.utils.utils import load_history, load_system_prompt_cached, load_emotion_prompt, load_dialogue_prompt, logger
 from module.params import OPENAI_MODEL, OPENAI_TEMPERATURE, OPENAI_TOP_P, OPENAI_MAX_TOKENS
 from module.mongo.emotion_dataset import get_recent_dialogue_history
-from module.response.main_response import collect_all_category_responses
+from module.response.response_loader import collect_all_category_responses
 
 #from module.memory.oblivion_emotion import clean_old_emotions
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
