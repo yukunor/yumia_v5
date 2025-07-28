ff0823f9a2884a2e23ff12224385c380bf033fb6
yukunor <noriyukikondo99@outlook.jp>
2025-07-28 21:53:30 +0900

    Update response_index.py

--- module/response/response_index.py
+++ module/response/response_index.py
@@ -7,6 +7,7 @@ from bson import ObjectId
 from module.utils.utils import logger
 from module.mongo.mongo_client import get_mongo_client
 from module.llm.llm_client import generate_gpt_response_from_history
+from module.params import emotion_map
 
 # 構成比とキーワードを受け取る検索インターフェース
 # Search interface that receives composition and keywords
