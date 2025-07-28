f5ade98359ffabb54d2c340bb61e097c0a793b6b
yukunor <noriyukikondo99@outlook.jp>
2025-07-28 21:49:55 +0900

    Update response_short.py

--- module/response/response_short.py
+++ module/response/response_short.py
@@ -1,4 +1,10 @@
-# module/response/response_short.py
+#module/response/response_short.py
+import json
+from bson import ObjectId
+
+from module.mongo.mongo_client import get_mongo_client
+from module.response.response_index import find_best_match_by_composition
+from module.utils.utils import logger
 
 # MongoDBのemotion_dataから、categoryが"short"の全データを取得する。
 # Retrieve all data from MongoDB emotion_data where category is "short".
